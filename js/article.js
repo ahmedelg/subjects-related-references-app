28b2958fe7b98ddc315473068cfb0acdadd9b7c0
Ahmed Al-Gaied <46943991+ahmedelg@users.noreply.github.com>
2021-12-17 18:33:09 +0200

    Add files via upload

---
+++ js/article.js
@@ -0,0 +1,26 @@
+// const allArticles = new Array();
+// const article = {
+//   showArticle: () => {
+
+//   },
+//   removeArticle: () => {
+
+//   },
+//   getArticles: () => {
+
+//   },
+//   addNewArticle: () => {
+
+//   },
+//   addName: () => {
+
+//   },
+//   addHrefs: () => {
+
+//   },
+//   updateArticleName: () => {
+
+//   },
+//   addNewHrefs: () => {
+//   }
+// }
